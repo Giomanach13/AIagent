74af799bba12b4999ea583b3740443856360d46e
Giomanach13 <tom.stas@skynet.be>
2025-11-05 15:18:00 +0100

    input

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+import os, sys
+from dotenv import load_dotenv
+from google import genai
+
 def main():
-    print("Hello from aiagent!")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <prompt>")
+        sys.exit(1)
+    prompt = sys.argv[1]
+
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents= prompt,
+    )
+    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print("Response:")
+    print(response.text)
 
 
 if __name__ == "__main__":
